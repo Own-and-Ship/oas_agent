04d999983e8555a73cce5c14216be05f314d18ff
Caius Durling <dev@caius.name>
2023-04-25 16:31:07 +0100

    Nobble Warning#warn with prepend over alias

--- lib/control/ruby_reporting.rb
+++ lib/control/ruby_reporting.rb
@@ -1,13 +1,17 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-module ::Warning
-  class << self
-    alias :original_warn :warn
+require "oas_agent/control"
 
-    def warn(warning)
-      original_warn(warning) unless OasAgent::AgentContext.config[:suppress_ruby_warnings]
-      OasAgent::AgentContext.agent.ruby_receiver.push(warning.strip, caller) if warning.include?("deprecated")
+module OasAgent
+  class Control
+    module RubyReporting
+      def warn(warning)
+        super unless unless OasAgent::AgentContext.config[:common][:suppress_ruby_warnings]
+        OasAgent::AgentContext.agent.ruby_receiver.push(warning.strip, caller) if warning.include?("deprecated")
+      end
     end
   end
 end
+
+::Warning.prepend(OasAgent::Control::RubyReporting)
