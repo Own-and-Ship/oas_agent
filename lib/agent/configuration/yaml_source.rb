55aafd1553dd90cb3b32c3ee8757618f309f9ffc
Will Jessop <will@willj.net>
2023-11-21 04:56:13 +0000

    Test that the config file is loaded and ensure aliases are processed

--- lib/agent/configuration/yaml_source.rb
+++ lib/agent/configuration/yaml_source.rb
@@ -1,27 +1,34 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+require "erb"
+require "yaml"
+
 module OasAgent
   module Agent
     module Configuration
       class YamlSource
-        def initialize(config_path, env)
+
+        attr_reader :logger
+
+        def initialize(config_path, env, logger=OasAgent::AgentContext.logger)
+          @logger = logger
           @config = {}
 
           begin
             expanded_config_file_path = File.expand_path(config_path)
             if config_path.empty? || !File.exist?(expanded_config_file_path)
-              OasAgent::AgentContext.logger.warn("There was no config file found at #{expanded_config_file_path}")
+              logger.warn("There was no config file found at #{expanded_config_file_path}")
               return
             end
 
-            OasAgent::AgentContext.logger.info("Reading configuration from #{expanded_config_file_path}")
+            logger.info("Reading configuration from #{expanded_config_file_path}")
 
             raw_file = File.read(expanded_config_file_path)
             erb_file = process_erb(raw_file)
             @config = process_yaml(erb_file, env, expanded_config_file_path)
           rescue ScriptError, StandardError => e
-            OasAgent::AgentContext.logger.info("Reading configuration from #{expanded_config_file_path}", e)
+            logger.info("Reading configuration from #{expanded_config_file_path}: #{e}")
           end
         end
 
@@ -39,16 +46,21 @@ module OasAgent
             file.gsub!(/^\s*#.*$/, '#')
             ERB.new(file).result(binding)
           rescue ScriptError, StandardError => e
-            OasAgent::AgentContext.logger.error("Failed ERB processing configuration file. This is typically caused by a Ruby error in <% %> templating blocks in your own_and_ship.yml file.", e)
+            logger.error("Failed ERB processing configuration file. This is typically caused by a Ruby error in <% %> templating blocks in your own_and_ship.yml file: #{e}")
             nil
           end
         end
 
         def process_yaml(file, env, path)
           if file
-            confighash = YAML.load(file)
+            confighash = if Psych::VERSION > "4.0"
+              YAML.safe_load(file, permitted_classes: [Symbol], aliases: true)
+            else
+              YAML.load(file)
+            end
+
             unless confighash.key?(env)
-              OasAgent::AgentContext.logger.error("Config file at #{path} doesn't include a '#{env}' section!")
+              logger.error("Config file at #{path} doesn't include a '#{env}' section!")
             end
 
             config = confighash[env] || {}
---
+++ test/lib/agent/configuration/yaml_source_test.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require "test_helper"
+
+class OasAgentRubyConfigurationYamlSourceTest < Minitest::Test
+  def test_loads_config_for_and_environment_including_aliases
+    log = StringIO.new
+    config = OasAgent::Agent::Configuration::YamlSource.new(
+      File.join(File.dirname(__FILE__), "yaml_samples", "with_aliases.yml"),
+      "production",
+      logger = Logger.new(log)
+    ).to_h
+
+    assert_equal "Someapp", config["app_name"]
+    assert config["enabled"]
+  end
+end
