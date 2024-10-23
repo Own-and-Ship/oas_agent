41693780e9b8b170a2c94ddf39de777920325251
Caius Durling <dev@caius.name>
2024-10-23 09:00:56 +0100

    Tidy up reporter handling background thread
    - `Reporter#close` now handles all the shutdown behaviour for the queue/thread.
    - `create_reporter_thread` returns the Thread object, not the at_exit Proc
    - We have *some* tests for threading management

--- test/lib/agent/reporter_test.rb
+++ test/lib/agent/reporter_test.rb
@@ -10,16 +10,62 @@ class OasAgentAgentReporterTest < Minitest::Test
 
     # Ensure we have default config in place
     OasAgent::AgentContext.config.integrate(OasAgent::Agent::Configuration::DefaultSource.new.to_h)
+
+    # Load the reporter in an isolated way
+    Object.const_set(:TestReporter, Class.new(OasAgent::Agent::Reporter))
   end
 
   def teardown
+    # Stop the temporary reporter
+    TestReporter.instance.close
+    Object.__send__(:remove_const, :TestReporter) if defined?(Reporter)
+
     # Remove the "stub"
     Object.__send__(:remove_const, :Rails) if defined?(Rails)
   end
 
   def test_instance_returns_same_object
-    first = OasAgent::Agent::Reporter.instance
-    second = OasAgent::Agent::Reporter.instance
-    assert_equal first.object_id, second.object_id
+    object1 = TestReporter.instance
+    object2 = TestReporter.instance
+
+    assert_equal object1.object_id, object2.object_id
+  end
+
+  def test_close_shuts_down_thread
+    thread = TestReporter.instance.instance_variable_get(:@reporter_thread)
+    queue = TestReporter.instance.instance_variable_get(:@report_queue)
+
+    TestReporter.instance.close
+
+    assert queue.closed?, "Reporter report queue should be closed"
+    refute thread.alive?, "Reporter thread should not be alive"
+  end
+
+  def test_close_shuts_down_slow_thread
+    logger = Class.new do
+      def messages
+        @messages ||= []
+      end
+
+      def warn(message)
+        self.messages << [:warn, message]
+      end
+    end
+
+    OasAgent::AgentContext.logger = logger.new
+
+    # Stop us leaking the original thread in test
+    TestReporter.instance.instance_variable_get(:@reporter_thread).kill
+
+    # Add a slow thread
+    slow_thread = Thread.new { sleep 10 }
+    TestReporter.instance.instance_variable_set(:@reporter_thread, slow_thread)
+
+    TestReporter.instance.close
+
+    assert_equal [[:warn, "Timeout joining report thread during shutdown"]], OasAgent::AgentContext.logger.messages
+    assert TestReporter.instance.instance_variable_get(:@report_queue).closed?, "Reporter report queue should be closed"
+
+    slow_thread.kill
   end
 end
