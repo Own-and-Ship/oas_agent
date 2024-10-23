f525cd23ab8adcc01473adc54aab014dd3a7fb25
Caius Durling <dev@caius.name>
2024-10-23 08:48:43 +0100

    Start testing Reporter

--- test/lib/agent/reporter_test.rb
+++ test/lib/agent/reporter_test.rb
@@ -1,4 +1,25 @@
 require "test_helper"
+require "support/mock_rails"
 
-class OasAgentRubyReceiverTest < Minitest::Test
+class OasAgentAgentReporterTest < Minitest::Test
+
+  def setup
+    # Stub it out and make sure it's reset before each test
+    Object.const_set(:Rails, MockRails)
+    MockRails.reset
+
+    # Ensure we have default config in place
+    OasAgent::AgentContext.config.integrate(OasAgent::Agent::Configuration::DefaultSource.new.to_h)
+  end
+
+  def teardown
+    # Remove the "stub"
+    Object.__send__(:remove_const, :Rails) if defined?(Rails)
+  end
+
+  def test_instance_returns_same_object
+    first = OasAgent::Agent::Reporter.instance
+    second = OasAgent::Agent::Reporter.instance
+    assert_equal first.object_id, second.object_id
+  end
 end
