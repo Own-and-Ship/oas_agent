da73156421bb58127f1276bad1d04a36fb0c80df
Caius Durling <dev@caius.name>
2024-10-31 00:43:41 +0000

    Add basic working spec for Default Source

---
+++ spec/agent/configuration/default_source_spec.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require "agent/configuration/default_source"
+
+RSpec.describe OasAgent::Agent::Configuration::DefaultSource do
+  describe "#to_h" do
+    subject { described_class.new.to_h }
+    it "returns a hash of defaults" do
+      expect(subject).to be_a(Hash)
+    end
+  end
+end
