3ea5522efefb6741593a7f8a06e3f857ac345c14
Caius Durling <dev@caius.name>
2024-11-24 17:33:41 +0000

    Finish porting event cache tests to rspec
    The event_count method feels wrong as a helper, but we don't publicly expose the count anywhere currently. This hides the reaching into the class and reading it's value at least.

--- spec/oas_agent/agent/event_cache_spec.rb
+++ spec/oas_agent/agent/event_cache_spec.rb
@@ -2,15 +2,47 @@
 # frozen_string_literal: true
 
 RSpec.describe OasAgent::Agent::EventCache do
+  let(:event_data) { ["fakehash", "Some deprecation message", "Ruby", "version 1", ["a.rb", "b.rb"], "/some/root"] }
+
+  subject(:event_cache) { described_class.new(*event_data) }
+
   describe ".hash_for_event_data" do
-    it "returns a hash"
+    subject(:result) { described_class.hash_for_event_data(*event_data[1..-1]) }
+
+    it "returns a hash" do
+      expect(result).to be_a(String)
+      expect(result.length).to eq(64)
+    end
+  end
+
+  describe "#new" do
+    it "starts counting at zero" do
+      expect(event_count).to eq(0)
+    end
   end
 
   describe "#increment" do
-    it "increments the count by 1"
+    subject(:increment) { event_cache.increment }
+
+    it "increments the count by 1" do
+      expect{ increment }.to change{ event_count }.by(1)
+    end
   end
 
   describe "#for_serialization" do
-    it "returns the event data as an array"
+    let(:stored_data) do
+      # For some reason we reverse these from method args to stored
+      # API expects them in this order, so output is correct
+      event_data.insert(3, event_data.delete_at(4))
+      event_data + [0]
+    end
+
+    it "returns the event data as an array" do
+      expect(event_cache.for_serialization).to eq(stored_data)
+    end
+  end
+
+  def event_count
+    event_cache.instance_variable_get(:@event_data)[described_class::DATA_INDEXES[:counts]]
   end
 end
